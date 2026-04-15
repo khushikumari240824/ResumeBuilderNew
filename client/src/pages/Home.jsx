d5a9ce857e2303a373d473ef4b412a86a8837a1c
Khushi Kumari <khushikumari240824@gmail.com>
2026-04-15 23:24:39 +0530

    Initial commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Banner from '../components/home/Banner'
+import Hero from '../components/home/Hero'
+import Features from '../components/Features'
+
+const Home = () => {
+  return (
+    <div>
+        <Banner/>
+        <Hero/>
+        <Features/>
+    </div>
+  
+  )
+}
+
+export default Home
---
+++ client/src/pages/ResumeBuilder.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ResumeBuilder = () => {
+  return (
+    <div>
+        <h1>
+            ResumeBuilder Page
+        </h1>
+    </div>
+  )
+}
+
+export default ResumeBuilder
