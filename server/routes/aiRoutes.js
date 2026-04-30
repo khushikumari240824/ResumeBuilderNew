bf1bbb7a12e9bfb309af6a681315607409cce83d
Khushi Kumari <khushikumari240824@gmail.com>
2026-04-30 19:49:05 +0530

    routes

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,15 +1,15 @@
-// import express from "express";
-// import protect from "../middlewares/authMiddleware.js";
-// import {
-//   enhanceJobDescription,
-//   enhanceProfessionalSummary,
-//   uploadResume,
-// } from "../controllers/aiController.js";
+import express from "express";
+import protect from "../middlewares/authMiddleware.js";
+import {
+  enhanceJobDescription,
+  enhanceProfessionalSummary,
+  uploadResume,
+} from "../controllers/aiController.js";
 
-// const aiRouter = express.Router();
+const aiRouter = express.Router();
 
-// aiRouter.post("/enhance-pro-sum", protect, enhanceProfessionalSummary);
-// aiRouter.post("/enhance-job-desc", protect, enhanceJobDescription);
-// aiRouter.post("/upload-resume", protect, uploadResume);
+aiRouter.post("/enhance-pro-sum", protect, enhanceProfessionalSummary);
+aiRouter.post("/enhance-job-desc", protect, enhanceJobDescription);
+aiRouter.post("/upload-resume", protect, uploadResume);
 
-// export default aiRouter;
+export default aiRouter;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,17 +1,17 @@
-// import express from "express";
-// import {
-//   getUserById,
-//   getUserResumes,
-//   loginUser,
-//   registerUser,
-// } from "../controllers/userController.js";
-// import protect from "../middlewares/authMiddleware.js";
+import express from "express";
+import {
+  getUserById,
+  getUserResumes,
+  loginUser,
+  registerUser,
+} from "../controllers/userController.js";
+import protect from "../middlewares/authMiddleware.js";
 
-// const userRouter = express.Router();
+const userRouter = express.Router();
 
-// userRouter.post("/register", registerUser);
-// userRouter.post("/login", loginUser);
-// userRouter.get("/data", protect, getUserById);
-// userRouter.get("/resumes", protect, getUserResumes);
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/data", protect, getUserById);
+userRouter.get("/resumes", protect, getUserResumes);
 
-// export default userRouter;
+export default userRouter;
